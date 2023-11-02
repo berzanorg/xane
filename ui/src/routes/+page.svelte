339cd5ba5a88c6245dedb756bde521db0990f498
Berzan <berzan@tutanota.com>
2023-11-02 14:41:12 +0000

    put: content

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { onMount } from 'svelte';
+	import Section from '$lib/components/Section.svelte';
+	import { wallet } from '$lib/stores/wallet';
 
 	onMount(async () => {
 		//const { TokenX } = await import('../../../contracts/build/src/');
@@ -12,4 +14,17 @@
 	<title>Xane - Decentralized Exchange</title>
 </svelte:head>
 
-<main class="main" />
+<main class="flex flex-col">
+	<Section>
+		<h2 class="text-2xl font-bold">Network</h2>
+		<p class="font-semibold text-amber-500">
+			{$wallet.network ?? 'Auro Wallet is not connected'}
+		</p>
+	</Section>
+	<Section>
+		<h2 class="text-2xl font-bold">Address</h2>
+		<p class="font-mono text-xs sm:text-base text-amber-500">
+			{$wallet.address ?? 'Auro Wallet is not connected'}
+		</p>
+	</Section>
+</main>
