ceb9a987ddcaab06beb2f10b33941c1ebcdeeb0a
Berzan <berzan@tutanota.com>
2023-11-02 15:08:03 +0000

    update: base path

--- ui/src/lib/components/Header.svelte
+++ ui/src/lib/components/Header.svelte
@@ -1,10 +1,11 @@
 <script>
+	import { base } from '$app/paths';
 	import ConnectButton from './ConnectButton.svelte';
 </script>
 
 <header
 	class="sticky top-0 z-30 flex items-center justify-between px-4 border-b md:px-6 h-14 bg-neutral-900 border-neutral-700"
 >
-	<a href="/" class="text-xl font-bold">Xane</a>
+	<a href="{base}/" class="text-xl font-bold">Xane</a>
 	<ConnectButton />
 </header>
