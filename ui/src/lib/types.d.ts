f6edc45c5d445a5b59302f56ffaa37c9f5a9a1d7
Berzan <berzan@tutanota.com>
2023-11-02 14:13:42 +0000

    extend: type of `window`

---
+++ ui/src/lib/types.d.ts
@@ -0,0 +1,18 @@
+// I extend the type of `window` to have type safety while accessing `window.mina`.
+declare interface Window {
+    /** 
+     * The object Auro Wallet injects to allow websites to interact.
+     * 
+     * Learn more: https://docs.aurowallet.com/general/ 
+     */
+    mina?: {
+        /** Makes a request to connect wallet and returns an array of addresses. Throws if user rejects. */
+        requestAccounts(): Promise<Array<string>>
+        /** Makes a request to connect wallet and returns an array of addresses. Sends an empty array if user rejects. */
+        getAccounts(): Promise<Array<string>>
+        /** Returns the name of the selected network. */
+        requestNetwork(): Promise<'Mainnet' | 'Devnet' | 'Berkeley' | 'Testworld2' | 'Unknown'>
+        /** Listens for specified event using given handler. Currently only `accountsChanged` & `chainChanged` are supported. */
+        on<T extends 'accountsChanged' | 'chainChanged'>(eventName: T, handler: (params: T extends 'accountsChanged' ? Array<string> : T extends 'chainChanged' ? string : never) => void): void
+    }
+}
