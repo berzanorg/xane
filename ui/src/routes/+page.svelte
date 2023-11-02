8e348425f4bf05d315e2d440d633d79674624bbd
Berzan <berzan@tutanota.com>
2023-11-02 18:46:03 +0000

    deploy: contract to `Testworld2` network

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,13 +1,35 @@
-<script>
+<script lang="ts">
 	import { onMount } from 'svelte';
 	import Section from '$lib/components/Section.svelte';
 	import { wallet } from '$lib/stores/wallet';
+	import { PublicKey } from 'o1js';
+	import { TokenX } from '../../../contracts/build/src/TokenX';
+
+	let zkApp: TokenX;
 
 	onMount(async () => {
-		//const { TokenX } = await import('../../../contracts/build/src/');
-		//const zkAppAddress = '';
-		//const zkApp = new TokenX(PublicKey.fromBase58(zkAppAddress))
+		const zkAppAddress = 'B62qoSKd89ksWtxN1b2zo1TZSLegB3Xaj6PAEGh2xg9fLgoZdhAeN1T';
+		zkApp = new TokenX(PublicKey.fromBase58(zkAppAddress));
+		console.log(zkApp.totalAmountInCirculation);
 	});
+
+	async function mintTokens() {
+		//todo: implement contract interaction here
+		// if (!$wallet.address) return alert('Not Connected');
+		// const zkAppPrivateKey = null as never;
+		// const deployerPrivateKey = null as never;
+		// const receiver = PublicKey.fromBase58($wallet.address);
+		// const amount = UInt64.from(42);
+		// const signature = Signature.create(
+		// 	zkAppPrivateKey,
+		// 	amount.toFields().concat(receiver.toFields())
+		// );
+		// const tx = await Mina.transaction(receiver, () => {
+		// 	zkApp.mint(receiver, amount, signature);
+		// });
+		// await tx.prove();
+		// await tx.sign([deployerPrivateKey]).send();
+	}
 </script>
 
 <svelte:head>
@@ -26,5 +48,15 @@
 		<p class="font-mono text-xs sm:text-base text-amber-500">
 			{$wallet.address ?? 'Auro Wallet is not connected'}
 		</p>
+	</Section>
+	<Section>
+		<h2 class="text-2xl font-bold">Mint Tokens</h2>
+		<button
+			disabled
+			class="h-8 px-6 font-semibold duration-200 rounded-full disabled:cursor-not-allowed bg-amber-600 disabled:hover:bg-amber-600 disabled:hover:scale-100 hover:bg-amber-500 active:bg-amber-500 hover:scale-95 active:scale-90"
+			on:click={mintTokens}
+		>
+			Mint
+		</button>
 	</Section>
 </main>
