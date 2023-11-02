1254240e34035f439ecd2a26867527e09384fd0a
Berzan <berzan@tutanota.com>
2023-11-02 14:39:45 +0000

    create: `Header` component

---
+++ ui/src/lib/components/Header.svelte
@@ -0,0 +1,10 @@
+<script>
+	import ConnectButton from './ConnectButton.svelte';
+</script>
+
+<header
+	class="sticky top-0 z-30 flex items-center justify-between px-4 border-b md:px-6 h-14 bg-neutral-900 border-neutral-700"
+>
+	<a href="/" class="text-xl font-bold">Xane</a>
+	<ConnectButton />
+</header>
