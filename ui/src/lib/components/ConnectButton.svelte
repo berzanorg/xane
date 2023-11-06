3e8d1d8c0895e8853f1983f1967d6d35634e5c6e
Berzan <berzan@tutanota.com>
2023-11-06 10:32:22 +0000

    change: style

--- ui/src/lib/components/ConnectButton.svelte
+++ ui/src/lib/components/ConnectButton.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import { wallet } from '$lib/stores/wallet';
+	import { wallet } from '$lib/stores/wallet'
 </script>
 
 <button
 	on:click={wallet.connect}
 	disabled={$wallet.isConnected}
-	class="h-8 px-4 font-bold duration-200 rounded-full bg-amber-600 hover:bg-amber-500 active:bg-amber-500 hover:scale-95 active:scale-90 disabled:cursor-not-allowed disabled:hover:bg-amber-600 disabled:hover:scale-100"
+	class="h-10 px-5 font-bold text-black duration-150 bg-white rounded-xlg hover:scale-95 active:scale-85 disabled:cursor-not-allowed disabled:hover:scale-100 disabled:active:scale-100"
 >
 	{#if $wallet.isConnected}
 		Connected
