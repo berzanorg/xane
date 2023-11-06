ac1fd3e75f41db182e4b45a8bf3267d818f93c29
Berzan <berzan@tutanota.com>
2023-11-06 10:33:23 +0000

    add: links

--- ui/src/lib/components/Header.svelte
+++ ui/src/lib/components/Header.svelte
@@ -1,11 +1,16 @@
 <script>
-	import { base } from '$app/paths';
-	import ConnectButton from './ConnectButton.svelte';
+	import ConnectButton from './ConnectButton.svelte'
+	import Nav from './Nav.svelte'
 </script>
 
-<header
-	class="sticky top-0 z-30 flex items-center justify-between px-4 border-b md:px-6 h-14 bg-neutral-900 border-neutral-700"
->
-	<a href="{base}/" class="text-xl font-bold">Xane</a>
-	<ConnectButton />
+<header class="sticky top-0 z-30 flex items-center justify-between h-16 px-4 bg-black lg:px-6">
+	<div class="flex w-40">
+		<a href="/" class="text-2xl font-bold">Xane</a>
+	</div>
+	<div class="hidden lg:block">
+		<Nav />
+	</div>
+	<div class="flex justify-end w-40">
+		<ConnectButton />
+	</div>
 </header>
