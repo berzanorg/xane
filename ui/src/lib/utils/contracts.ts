6b7bb2f476074d171850221c0b8d84f600d6aaae
Berzan <berzan@tutanota.com>
2023-11-07 16:14:45 +0000

    define: `deployTokenContract` function

--- ui/src/lib/utils/contracts.ts
+++ ui/src/lib/utils/contracts.ts
@@ -1,90 +1,44 @@
-import { AccountUpdate, Mina, PrivateKey, PublicKey, } from "o1js"
-import { Token } from "xane-contracts"
+import { AccountUpdate, Mina, PrivateKey, PublicKey, UInt64, } from "o1js"
+import { Token, utils } from "xane-contracts"
 import { wallet } from "$lib/stores/wallet"
-//import verificationKey2 from './verificationKey.json'
+import verificationKey from './verificationKey.json'
 
-interface CreateTokenArgs {
-    signer: string | null
+interface DeployTokenArgs {
+    signer: string
+    supply: number
     ticker: string
     name: string
-    decimals: number
-    supplyMaximum: bigint
 }
 
 
-export const createToken = async (args: CreateTokenArgs) => {
-    if (args.signer === null) return
 
+
+export const deployTokenContract = async (args: DeployTokenArgs) => {
     const signer = PublicKey.fromBase58(args.signer)
+    const contractPrivateKey = PrivateKey.random()
+    const contractPublicKey = contractPrivateKey.toPublicKey()
+    const contract = new Token(contractPublicKey)
 
+    const name = utils.stringToField(args.name)
+    const ticker = utils.stringToField(args.ticker)
+    const supply = UInt64.from(args.ticker)
 
-    console.log('compilation started')
-    const { verificationKey } = await Token.compile()
-    console.log('compilation complete')
-
-
-    const zkAppPrivateKey = PrivateKey.random()
-    const zkAppPublicKey = zkAppPrivateKey.toPublicKey()
-    // const adminSignature = Signature.create(
-    //     zkAppPrivateKey,
-    //     ticker.toFields().concat(name.toFields()).concat(decimals.toFields()).concat(supplyMaximum.toFields())
-    // )
-
-    const tx = await Mina.transaction(signer, () => {
-        const token = new Token(zkAppPublicKey)
+    let tx = await Mina.transaction(signer, () => {
         AccountUpdate.fundNewAccount(signer)
-        token.deploy({ verificationKey, zkappKey: zkAppPrivateKey })
+        contract.deploy({
+            verificationKey,
+            zkappKey: contractPrivateKey,
+            name,
+            supply,
+            ticker,
+        })
     })
+
     await tx.prove()
 
-    const tx2 = tx.sign([zkAppPrivateKey])
+    tx = tx.sign([contractPrivateKey])
 
-    await wallet.sendTransaction(tx2.toJSON())
+    const hash = await wallet.sendTransaction(tx.toJSON())
 
-    return
-
-
-
-    // const contractPrivateKey = PrivateKey.random()
-    // const contractPublicKey = contractPrivateKey.toPublicKey()
-    // const contractInstance = new Token(contractPublicKey)
-
-    // const { verificationKey } = await Token.compile()
-
-    // const txnToDeploy = await Mina.transaction(signer, () => {
-    //     AccountUpdate.fundNewAccount(signer)
-    //     contractInstance.deploy({ verificationKey, zkappKey: contractPrivateKey })
-    // })
-
-    // await txnToDeploy.prove()
-
-    // const a = await wallet.sendTransaction(txnToDeploy.toJSON())
-
-    // console.log(a)
-
-    // return
-    // const ticker = Field.from(0)
-    // const name = Field.from(0)
-    // const decimals = UInt32.from(args.decimals)
-    // const supplyMaximum = UInt64.from(args.supplyMaximum)
-    // const adminSignature = Signature.create(
-    //     contractPrivateKey,
-    //     ticker.toFields().concat(name.toFields()).concat(decimals.toFields()).concat(supplyMaximum.toFields())
-    // )
-
-
-
-    // const txnToInit = await Mina.transaction(signer, () => {
-    //     contractInstance.initMetadata(ticker, name, decimals, supplyMaximum, adminSignature)
-    // })
-
-    // const txns = {
-    //     deployTransaction: txnToDeploy.toJSON(),
-    //     initTransaction: txnToInit.toJSON(),
-    // }
-
-    // console.log(txns)
-
-    // await wallet.sendTransaction(txns.deployTransaction)
-    // await wallet.sendTransaction(txns.initTransaction)
+    return hash
 }
