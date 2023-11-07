eb587bfbe167a63cbd7eb0aff94b3dc868e81f93
Berzan <berzan@tutanota.com>
2023-11-07 16:56:38 +0000

    impl: auto-connecting

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script>
 	import '../app.css'
 	import Header from '$lib/components/Header.svelte'
+	import { onMount } from 'svelte'
+	import { wallet } from '$lib/stores/wallet'
 	import Footer from '$lib/components/Footer.svelte'
 	import Main from '$lib/components/Main.svelte'
+
+	onMount(wallet.connectIfAuthorized)
 </script>
 
 <Header />
