58daf94efa572fecd37a6d0de21191ffaf726dea
Berzan <berzan@tutanota.com>
2023-11-10 14:50:20 +0000

    define: `Exchange` struct

--- backend/src/exchange.rs
+++ backend/src/exchange.rs
@@ -1,5 +1,46 @@
-/// # Exchange
-/// This struct represents the exchange itself.
+use anyhow::{anyhow, Ok, Result};
+
+use crate::{mina::Field, pair::Pair};
+use std::collections::HashMap;
+
+/// Represents the exchange itself.
 ///
-/// It contains order books of each pair.
-pub struct Exchange {}
+/// Contains pairs in the exchange.
+pub struct Exchange {
+    /// Pairs in the exchange.
+    pairs: HashMap<Field, Pair>,
+}
+
+impl Exchange {
+    /// Creates a new instance of `Exchange`.
+    ///
+    /// # Usage
+    ///
+    /// ```rs
+    /// let exchange = Exchange::new();
+    /// ```
+    pub fn new() -> Exchange {
+        Exchange {
+            pairs: HashMap::new(),
+        }
+    }
+
+    /// Adds a new pair to the exchange.
+    ///
+    /// # Usage
+    ///
+    /// ```rs
+    /// exchange.add_pair(name, hash_of_token_ids);
+    /// ```
+    pub fn add_pair(&mut self, name: String, hash_of_token_ids: Field) -> Result<()> {
+        if self.pairs.contains_key(&hash_of_token_ids) {
+            return Err(anyhow!("Pair already exists: {}", name));
+        }
+
+        let pair = Pair::new(name);
+
+        self.pairs.insert(hash_of_token_ids, pair);
+
+        Ok(())
+    }
+}
