dd03e1eea2f4601fee310508d1a848ca9d776767
Berzan <berzan@tutanota.com>
2023-12-15 20:05:19 +0000

    create: new backend

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import { App } from '@tinyhttp/app'
+import { Encoding } from 'o1js'
+import  { Token } from 'xane'
+
+const app = new App()
+
+app.get('/', (_req, res) => {
+    const fields = Encoding.stringToFields('hello world')
+    const message = Encoding.stringFromFields(fields)
+
+    console.log(Token)
+    
+    res.send(message)
+})
+
+app.listen(3000, () => console.log('Started on http://localhost:3000'))
