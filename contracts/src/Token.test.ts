d37d232283a5dd634956e783f2c7fcc4054be4fe
Berzan <berzan@tutanota.com>
2023-12-18 16:28:26 +0000

    update: `Token` smart contract

--- contracts/src/Token.test.ts
+++ contracts/src/Token.test.ts
@@ -1,62 +1,88 @@
-import { AccountUpdate, Field, Mina, PrivateKey, PublicKey, Signature, UInt32, UInt64, Encoding } from 'o1js'
+import { AccountUpdate, Field, Mina, PrivateKey, PublicKey, UInt64, Encoding } from 'o1js'
 import { Token } from './Token'
-
 
 const proofsEnabled = false
 
 describe('Token Contract', () => {
-  let deployerPrivateKey: PrivateKey
-  let deployerPublicKey: PublicKey
-
-  let zkAppInstance: Token
-  let zkAppPrivateKey: PrivateKey
-  let zkAppPublicKey: PublicKey
-
-  let verificationKey: { data: string, hash: Field }
-
-  beforeAll(async () => {
     const Local = Mina.LocalBlockchain({ proofsEnabled })
     Mina.setActiveInstance(Local)
 
-    deployerPrivateKey = Local.testAccounts[0].privateKey
-    deployerPublicKey = Local.testAccounts[0].publicKey
+    const deployerPrivateKey: PrivateKey = Local.testAccounts[0].privateKey
+    const deployerPublicKey: PublicKey = Local.testAccounts[0].publicKey
+    const tokenZkAppPrivateKey: PrivateKey = PrivateKey.random()
+    const tokenZkAppPublicKey: PublicKey = tokenZkAppPrivateKey.toPublicKey()
+    const token: Token = new Token(tokenZkAppPublicKey)
 
-    zkAppPrivateKey = PrivateKey.random()
-    zkAppPublicKey = zkAppPrivateKey.toPublicKey()
-    zkAppInstance = new Token(zkAppPublicKey)
+    let verificationKey: { data: string; hash: Field }
 
-    verificationKey = (await Token.compile()).verificationKey
-  })
-
-  it('can create a new token', async () => {
-    const ticker = Encoding.stringToFields('MY')[0]
-    const name = Encoding.stringToFields('My Token')[0]
-    const supply = UInt64.from(100_000_000)
-
-    const tx = await Mina.transaction(deployerPublicKey, () => {
-      AccountUpdate.fundNewAccount(deployerPublicKey)
-      AccountUpdate.fundNewAccount(deployerPublicKey)
-      zkAppInstance.deploy({ verificationKey, zkappKey: zkAppPrivateKey, ticker, name, supply })
+    beforeAll(async () => {
+        verificationKey = (await Token.compile()).verificationKey
     })
 
-    await tx.prove()
-    await tx.sign([deployerPrivateKey, zkAppPrivateKey]).send()
-  })
+    it('can create a new token', async () => {
+        const symbol = Encoding.stringToFields('MY')[0]
+        const fixedSupply = UInt64.from(100_000_000)
 
+        const tx = await Mina.transaction(deployerPublicKey, () => {
+            AccountUpdate.fundNewAccount(deployerPublicKey)
+            AccountUpdate.fundNewAccount(deployerPublicKey)
+            token.deploy({ verificationKey, zkappKey: tokenZkAppPrivateKey, symbol, fixedSupply })
+        })
 
-  it('can send and receive tokens', async () => {
-    const receiverAddress = PrivateKey.random().toPublicKey()
-    const amount = UInt64.from(20_000_000)
+        await tx.prove()
+        await tx.sign([deployerPrivateKey, tokenZkAppPrivateKey]).send()
 
-    const tx = await Mina.transaction(deployerPublicKey, () => {
-      AccountUpdate.fundNewAccount(deployerPublicKey)
-      zkAppInstance.sendTokens(deployerPublicKey, receiverAddress, amount)
+        expect(token.fixedSupply.get()).toEqual(fixedSupply)
+        expect(Mina.getBalance(deployerPublicKey, token.token.id)).toEqual(fixedSupply)
     })
 
-    await tx.prove()
-    await tx.sign([deployerPrivateKey]).send()
+    it('can send and receive tokens', async () => {
+        const receiverAddress = PrivateKey.random().toPublicKey()
+        const amount = UInt64.from(20_000_000)
 
-    expect(Mina.getBalance(deployerPublicKey, zkAppInstance.token.id)).toEqual(UInt64.from(80_000_000))
-    expect(Mina.getBalance(receiverAddress, zkAppInstance.token.id)).toEqual(amount)
-  })
+        const tx = await Mina.transaction(deployerPublicKey, () => {
+            AccountUpdate.fundNewAccount(deployerPublicKey)
+            token.transfer(deployerPublicKey, receiverAddress, amount)
+        })
+
+        await tx.prove()
+        await tx.sign([deployerPrivateKey]).send()
+
+        expect(Mina.getBalance(deployerPublicKey, token.token.id)).toEqual(UInt64.from(80_000_000))
+        expect(Mina.getBalance(receiverAddress, token.token.id)).toEqual(amount)
+    })
+
+    it("can't send and receive tokens if balance is not enough", async () => {
+        try {
+            const receiverAddress = PrivateKey.random().toPublicKey()
+            const amount = UInt64.from(90_000_000)
+
+            const tx = await Mina.transaction(deployerPublicKey, () => {
+                AccountUpdate.fundNewAccount(deployerPublicKey)
+                token.transfer(deployerPublicKey, receiverAddress, amount)
+            })
+
+            await tx.prove()
+            await tx.sign([deployerPrivateKey]).send()
+
+            throw 'this must have failed'
+        } catch (error) {}
+    })
+
+    it("can't send and receive tokens if not signed by the sender", async () => {
+        try {
+            const receiverAddress = PrivateKey.random().toPublicKey()
+            const amount = UInt64.from(10_000_000)
+
+            const tx = await Mina.transaction(deployerPublicKey, () => {
+                AccountUpdate.fundNewAccount(deployerPublicKey)
+                token.transfer(deployerPublicKey, receiverAddress, amount)
+            })
+
+            await tx.prove()
+            await tx.sign([]).send()
+
+            throw 'this must have failed'
+        } catch (error) {}
+    })
 })
--- contracts/src/Token.ts
+++ contracts/src/Token.ts
@@ -1,71 +1,76 @@
 import {
-    Bool,
     DeployArgs,
     Field,
     Permissions,
     PublicKey,
-    Signature,
     SmartContract,
     State,
-    UInt32,
     UInt64,
     method,
     state
 } from "o1js"
 
 interface CustomDeployArgs {
-    name: Field
-    ticker: Field
-    supply: UInt64
+    symbol: Field
+    fixedSupply: UInt64
 }
 
 /**
- * This smart contract is a basic token standard.
  * 
- * This token standard will be used in development phase
+ * # `Token` Smart Contract
  * 
- * When an official token standard is released, this smart contract will be unnecessary.
+ * The smart contract for tokens on Xane.
+ * 
+ * **Note:** `deploy` method requires two account funding. 
+ * 
+ * # Usage
+ * 
+ * ```ts
+ * // Import `Token` smart contract.
+ * import { Token } from 'xane'
+ * 
+ * // Create an instance of `Token` contract.
+ * const token = new Token(zkAppPublicKey)
+ * 
+ * // Deploy it with given symbol and supply.
+ * token.deploy({ verificationKey, zkappKey, symbol, fixedSupply })
+ * 
+ * // Transfer tokens.
+ * token.transfer(receiver, amount)
+ * 
+ * ```
+ * 
  */
 export class Token extends SmartContract {
-    /** This state represents the name of the token. */
-    @state(Field) name = State<Field>()
+    @state(Field) symbol = State<Field>()
 
-    /** This state represents the ticker of the token. */
-    @state(Field) ticker = State<Field>()
+    @state(UInt64) fixedSupply = State<UInt64>()
 
-    /** This state represents the supply of the token. */
-    @state(UInt64) supply = State<UInt64>()
-
-    /** Deploys the smart contract. */
     deploy(args: DeployArgs & CustomDeployArgs) {
         super.deploy(args)
 
-        const permissionToEdit = Permissions.proof()
-
         this.account.permissions.set({
             ...Permissions.default(),
-            editState: permissionToEdit,
-            setTokenSymbol: permissionToEdit,
-            send: permissionToEdit,
-            receive: permissionToEdit,
+            editState: Permissions.proof(),
+            setTokenSymbol: Permissions.proof(),
+            send: Permissions.proof(),
+            receive: Permissions.proof(),
         })
 
-        this.name.set(args.name)
-        this.ticker.set(args.ticker)
-        this.supply.set(args.supply)
+        this.symbol.set(args.symbol)
 
         this.token.mint({
             address: this.sender,
-            amount: args.supply
+            amount: args.fixedSupply
         })
+        
+        this.fixedSupply.set(args.fixedSupply)
     }
 
-    /** Sends given amount of tokens to the receiver address from the sender address. */
-    @method sendTokens(senderAddress: PublicKey, receiverAddress: PublicKey, amount: UInt64) {
-        // Sends given amount of tokens from sender to receiver.
+    @method transfer(from: PublicKey, to: PublicKey, amount: UInt64) {
         this.token.send({
-            from: senderAddress,
-            to: receiverAddress,
+            from,
+            to,
             amount
         })
     }
