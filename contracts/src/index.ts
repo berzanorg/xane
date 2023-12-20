de12a0ce25a436817ad12255345eeb4ddfa06a6f
Berzan <berzan@tutanota.com>
2023-12-20 16:51:43 +0000

    export: new things

--- contracts/src/index.ts
+++ contracts/src/index.ts
@@ -1,3 +1,3 @@
 import { Token } from './Token.js'
-import { Exchange, AUTHORITY_PRIVATE_KEY } from './Exchange.js'
-export { Token, Exchange, AUTHORITY_PRIVATE_KEY }
+import { Exchange, AUTHORITY_PRIVATE_KEY, ORDERS_HEIGHT, PAIRS_HEIGHT, PairWitness, OrderWitness } from './Exchange.js'
+export { Token, Exchange, AUTHORITY_PRIVATE_KEY, ORDERS_HEIGHT, PAIRS_HEIGHT, PairWitness, OrderWitness }
