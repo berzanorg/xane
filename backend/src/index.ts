aa3df0221d52869f427ff03be6e22655312d7a25
Berzan <berzan@tutanota.com>
2023-12-21 14:05:50 +0000

    add: routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,34 @@
 import { App } from '@tinyhttp/app'
-import { Encoding } from 'o1js'
-import { Token } from 'xane'
+import { PrivateKey } from 'o1js'
+import { Exchange } from 'xane'
 import { Database } from './database'
+
+const SAMPLE_PUBLIC_KEY = PrivateKey.random().toPublicKey()
 
 const db = new Database()
 
 const app = new App()
 
-app.get('/', (_req, res) => {
-    const fields = Encoding.stringToFields('hello world')
-    const message = Encoding.stringFromFields(fields)
+const contract = new Exchange(SAMPLE_PUBLIC_KEY)
 
-    console.log(Token)
+app.post('/create-pair', (_req, res) => {
+    // todo
+})
 
-    res.send(message)
+app.post('/place-order', (_req, res) => {
+    // todo
+})
+
+app.post('/cancel-order', (_req, res) => {
+    // todo
+})
+
+app.post('/execute-order', (_req, res) => {
+    // todo
+})
+
+app.get('/order-book', (_req, res) => {
+    // todo
 })
 
 app.listen(3000, () => console.log('Started on http://localhost:3000'))
