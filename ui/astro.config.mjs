39cb606374f868f3d020f7967eefab83604750ca
Berzan <berzan@tutanota.com>
2023-12-25 11:17:06 +0000

    fix: COOP & COEP issues

--- ui/astro.config.mjs
+++ ui/astro.config.mjs
@@ -1,9 +1,28 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import solidJs from '@astrojs/solid-js'
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), solidJs()],
+    vite: {
+        plugins: [
+            {
+                configureServer: (server) => {
+                    server.middlewares.use((_req, res, next) => {
+                        res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+                        res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
+                        next()
+                    })
+                },
+            },
+        ],
+        build: {
+            target: 'esnext',
+        },
+        worker: {
+            format: 'es',
+        },
+        optimizeDeps: { esbuildOptions: { target: 'esnext' } },
+    },
 })
